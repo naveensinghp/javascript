79f9fb841b4fd56735b7402b81796edc24d77ae1
Naveen Singh <30367189+naveensinghp@users.noreply.github.com>
2021-01-29 00:02:48 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,36 @@
+//1. JS Primitive Data Type
+ // string
+ // number 
+ // boolean 
+ // null 
+ // undefined 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ var person = 
+ {
+     firstname: 'Naveen',
+     lastname: 'Singh',
+     age:30,
+     school :{
+         sslc: 'metro',
+         hsc: 'madonna'
+     },
+     college: ['SNS','PSG','CIET']
+ }
+
+var x = person 
+x.age = 10
+
+console.log(x);
