207f4ce7ababffa6d9d119713e0974e068177f90
Naveen Singh <30367189+naveensinghp@users.noreply.github.com>
2021-01-30 01:01:48 +0530

    test

--- obj.js
+++ obj.js
@@ -5,32 +5,40 @@
  // null 
  // undefined 
 
+const obj = {name:'naveen',age:25,job:{ google:'USA',apple:'Califorina'} }
+
+const name = 'Arya'
+const age = 22
+const job = { google : 'USA', apple: 'Califorina'}
 
 
+const obj2 = {
+    heroname: name,
+    heroage: age,
+    oldjobs: job
+}
 
 
+const obj3 = { name, age, job}
 
 
+let o = {
+  property: function (parameters) {},
+  get property() {},
+  set property(value) {}
+};
+console.log(o);
 
 
+let number = [1,2,34,5];
+let test = number.reverse()
+console.log(test);
 
 
- 
+let numbertest = [];
 
+for(let i=number; number.length -1; i >=0 ){
+  numbertest.push(number[i])
+}
 
- var person = 
- {
-     firstname: 'Naveen',
-     lastname: 'Singh',
-     age:30,
-     school :{
-         sslc: 'metro',
-         hsc: 'madonna'
-     },
-     college: ['SNS','PSG','CIET']
- }
-
-var x = person 
-x.age = 10
-
-console.log(x);
+console.log(numbertest)
